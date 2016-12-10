d45b4882225763ca1cc57cd460a1cd5e100a690b
JHenderson219 <hendersonj219@gmail.com>
2016-12-10 12:30:47 -0800

    Working on Reset, appears but doesn't dissapear

--- scripts/ticTacToeScript.js
+++ scripts/ticTacToeScript.js
@@ -1,26 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	var playerSide, computerSide;
+	var playerSide, computerSide; 
+	var selectorVisible=true;
 	function reset(){
+		$("#resetButton").attr("disabled","disabled");
+		$("#resetButton").addClass("animated fadeOut");
+		var resetTimeout = window.setTimeout(function(){
+			$("#resetButton").hide();
+		},1000);
+		if(selectorVisible){
 
+		}else{
+
+		}
 	}
 	function hideSelector(){
-
-	}
-	function showSelector(){
-
-	}
-	function playerSelectsSide(side){
+		selectorVisible=false;
 		$("#playerX").attr("disabled","disabled");
 		$("#playerO").attr("disabled","disabled");
 		$("#playerSelector").addClass("animated fadeOut");
 		$("#selectorText").addClass("animated fadeOut");
-	var selectorTimeout = window.setTimeout(function(){
+		var selectorTimeout = window.setTimeout(function(){
 			$("#selectorArea").append("<button class = 'animated fadeIn btn btn-secondary btn-lg center-block' id='resetButton'>Reset Game</button>");
-			$("#selectorText").css("visibility","hidden");	
+			$("#selectorText").css("visibility","hidden");
+			$("#playerSelector").css("visibility","hidden");	
 			$("#playerSelector").hide();
-	},1000);
+		},1000);
+	}
+	function showSelector(){
+		selectorVisible=true;
 
-
+	}
+	function playerSelectsSide(side){
+		hideSelector();
 	}
 
 	$("#playerX").on("click",function(){
@@ -30,5 +41,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	$("#playerO").on("click",function(){
 		console.log ("player O clicked!")
 		playerSelectsSide("O");
+	});
+	$("#resetButton").on("click",function(){
+		console.log("reset button clicked!");
+		reset();
 	});
 });
