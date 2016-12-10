946977e954cbb2d4cc6a13537ab652e9175c78c4
JHenderson219 <hendersonj219@gmail.com>
2016-12-09 19:50:12 -0800

    12/9 final update
    -Get fadeout and removal of selector buttons to work.
    -Make functions to hide and show selectors
    -make areas clickable
    -make clicks add x's or o'x

--- scripts/ticTacToeScript.js
+++ scripts/ticTacToeScript.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	
+	function hideSelector(){
+
+	}
+	function showSelector(){
+
+	}
+	function playerSelectsX(){
+		$("#playerSelector").addClass("animated fadeOut");
+		setTimeout(function(){
+			$("#playerSelector").hide();
+		},1000); 
+	}
+
+	function playerSelectsO(){
+
+	}
+
+	$("#playerX").on("click",function(){
+		playerSelectsX();
+	});
+	$("#playerO").on("click",function(){
+		playerSelectsO();
+	});
 });
